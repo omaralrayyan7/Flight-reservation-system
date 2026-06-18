3b8e257ffe4e1ec0072f3714ccf8e1f06128c469
omar alrayyan <omaralrayyan7@gmail.com>
2026-06-18 07:32:15 +0300

    Add Models/ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace FlightReservationApp_f.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
