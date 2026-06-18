f62215942d057a121de74222fc7aaff04258b8b4
omar alrayyan <omaralrayyan7@gmail.com>
2026-06-18 07:32:12 +0300

    Add Models/Admin.cs

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -2,18 +2,19 @@ using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 namespace FlightReservationApp_f.Models
-{
-    [Index(nameof(Admin.username), IsUnique = true)]
-    public class Admin
     {
-        [Key]
-        public int AdminId { get; set; }
-        public string Name { get; set; }
+        [Index(nameof(Admin.username), IsUnique = true)]
+        public class Admin
+        {
+            [Key]
+            public int AdminId { get; set; }
+            public string Name { get; set; }
+            [Required]
+            public string username { get; set; }
+            [Required]
+            public string password { get; set; }
 
-        [Required]
-        public string username { get; set; }
-
-        [Required]
-        public string password { get; set; }
+           
+        }
     }
-}
+
