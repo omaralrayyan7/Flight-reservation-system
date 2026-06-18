8988ed05a8658e1d257834897fe79d2fd9e79ada
omar alrayyan <omaralrayyan7@gmail.com>
2026-06-18 07:29:14 +0300

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using FlightReservationApp_f.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")
+        ?? throw new InvalidOperationException("Connection string 'DataContext' not found.")));
+
+builder.Services.AddControllersWithViews();
+
+// Enable session
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Flights}/{action=Index}/{id?}");
+
+app.Run();
