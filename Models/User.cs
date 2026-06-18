c8c278a90d02512ca80313afa2472603427758e5
omar alrayyan <omaralrayyan7@gmail.com>
2026-06-18 07:32:06 +0300

    Add Models/User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,23 +1,28 @@
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
 
-namespace FlightReservationApp_f.Models
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+    using System.ComponentModel.DataAnnotations;
+
+    namespace FlightReservationApp_f.Models
 {
-    [Index(nameof(User.username), IsUnique = true)]
-    public class User
-    {
-        [Key]
-        public int Id { get; set; }
+        [Index(nameof(User.username), IsUnique = true)]
+        public class User
+        {
+            [Key]
+            public int Id { get; set; }
 
-        [Required(ErrorMessage = "Should be filled")]
-        [StringLength(50)]
-        public string Name { get; set; }
+            [Required(ErrorMessage = "Should be insert")]
+            [StringLength(50)]
+            public string Name { get; set; }
 
-        [Required(ErrorMessage = "Should be filled")]
-        [StringLength(50)]
-        public string username { get; set; }
+            [Required(ErrorMessage = "Should be insert")]
+            [StringLength(50)]
+            public string username { get; set; }
 
-        [Required(ErrorMessage = "Should be filled")]
-        public string password { get; set; }
+            [Required(ErrorMessage = "Should be insert")]
+            public string password { get; set; }
+
+            
+        }
     }
-}
+
