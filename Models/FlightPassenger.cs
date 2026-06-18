ae1f0713d48105b65e3bc4ae77027d0ccf2b17a9
omar alrayyan <omaralrayyan7@gmail.com>
2026-06-18 07:32:04 +0300

    Add Models/FlightPassenger.cs

--- Models/FlightPassenger.cs
+++ Models/FlightPassenger.cs
@@ -4,14 +4,21 @@ namespace FlightReservationApp_f.Models
 {
     public class FlightPassenger
     {
+        // Primary key for the FlightPassenger entity
         public int Id { get; set; }
 
+        // Foreign key to the Flight entity
         [Required]
         public int FlightId { get; set; }
+
+        // Navigation property for the related Flight
         public Flight? Flight { get; set; }
 
+        // Foreign key to the Passenger entity
         [Required]
         public int PassengerId { get; set; }
+
+        // Navigation property for the related Passenger
         public Passenger? Passenger { get; set; }
     }
 }
