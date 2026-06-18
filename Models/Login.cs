97f3edf94dad4291fa33b2cab726a95288bef6c8
omar alrayyan <omaralrayyan7@gmail.com>
2026-06-18 07:32:13 +0300

    Add Models/Login.cs

--- Models/Login.cs
+++ Models/Login.cs
@@ -1,13 +1,14 @@
 using System.ComponentModel.DataAnnotations;
-
 namespace FlightReservationApp_f.Models
 {
-    public class Login
-    {
-        [Required(ErrorMessage = "*")]
-        public string username { get; set; }
+        public class Login
+        {
+            [Required(ErrorMessage = "*")]
+            public string username { get; set; }
 
-        [Required(ErrorMessage = "*")]
-        public string password { get; set; }
+            [Required(ErrorMessage = "*")]
+
+            public string password { get; set; }
+        }
     }
-}
+
