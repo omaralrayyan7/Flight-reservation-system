8a71ae490fe038b858f6e17af81a0ea9ff4589d3
omar alrayyan <omaralrayyan7@gmail.com>
2026-06-18 07:29:33 +0300

    Add Controllers/HomeController.cs

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using FlightReservationApp_f.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FlightReservationApp_f.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
