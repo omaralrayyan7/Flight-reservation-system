883c46986071ae0de25b6892dbad96bfbeccde44
omar alrayyan <omaralrayyan7@gmail.com>
2026-06-18 07:33:50 +0300

    Add Migrations/20250114094633_UpdateFlightModel.cs

---
+++ Migrations/20250114094633_UpdateFlightModel.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightReservationApp_f.Migrations.Data
+{
+    /// <inheritdoc />
+    public partial class UpdateFlightModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
